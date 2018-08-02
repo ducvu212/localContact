c96739057bd8614362e9574d58655e721843126f
ducvu212 <minhducvu.212@gmail.com>
2018-08-02 14:15:23 +0700

    init ui project

--- app/src/main/java/com/example/ducvu212/localcontact/MainActivity.java
+++ app/src/main/java/com/example/ducvu212/localcontact/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.ducvu212.localcontact;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TRANSACTION_ADD_TAG = "Tag";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFragment();
+    }
+
+    private void initFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.frame_main_layout, ContactFragment.newInstance(), TRANSACTION_ADD_TAG);
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
